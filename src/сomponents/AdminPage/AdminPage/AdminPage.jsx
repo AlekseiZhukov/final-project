9f3b798216d609d45e414ee99f86936a49a0ff61
Aleksey <56933174+AlekseiZhukov@users.noreply.github.com>
2020-10-23 15:13:57 +0300

    Add files via upload

---
+++ src/сomponents/AdminPage/AdminPage/AdminPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './AdminPage.module.css';
+
+const AdminPage = (props) => {
+    return (
+        <div>
+            Admin Page
+        </div>
+    )
+}
+export default AdminPage
