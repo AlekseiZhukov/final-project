b2fb124ce95c61aa9c0fe995203d3deb0fc062b1
AlekseiZhukov <alex__zhukov@mail.ru>
2020-10-24 11:50:39 +0300

    fix AdminPage

--- src/сomponents/AdminPage/AdminPage.jsx
+++ src/сomponents/AdminPage/AdminPage.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import style from './AdminPage.module.css';
+import TheftReportItem from "../TheftReportItem/TheftReportItem";
+import Preloader from "../common/Preloader/Preloader";
 
 const AdminPage = (props) => {
+    if (!props.state.theftReportsIsFetching) {
+
+        return <Preloader />
+    }
+
+    const reportItem = props.state.arrayTheftReports.map( r => <TheftReportItem
+        key={r._id}
+        numberReport={r.__v}
+        createdDate={r.createdAt}
+        statusReport={r.status}
+        description={r.description}
+        officer={''}
+        resolution={r.resolution}
+    />)
+
     return (
         <div>
-            Admin Page
+            <table>
+                <tr>
+                    <th>Сообщение №</th>
+                    <th>дата создания</th>
+                    <th>Статус сообщения</th>
+                    <th>описание</th>
+                    <th>ответственный сотрудник</th>
+                    <th>комментарий</th>
+                    <th>удалить</th>
+
+                </tr>
+                {reportItem}
+            </table>
+
         </div>
     )
 }
